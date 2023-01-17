b5566e06f85927d719cc2351f3d3cac9032f1fe2
gowsalya.narayan <gowsalya.narayan@Asplap2507.Aspiresys.com>
2023-01-17 07:55:19 +0530

    This is the first commit (conversion class) by TL

---
+++ src/com/aspire/demo/Conversion.java
@@ -0,0 +1,30 @@
+package com.aspire.demo;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Conversion {
+	  public static void main(String[] args) {
+
+	    // Array declaration and initialization
+	    String cityNames[]={"Agra", "Mysore", "Chandigarh", "Bhopal"};
+
+	    // Array to ArrayList conversion
+	    ArrayList<String> cityList= new ArrayList<String>(Arrays.asList(cityNames));
+	    
+	   // String array[] = new String[cityList.size()];// ArrayList to String Array conversion 
+	    
+
+	    // Adding new elements to the list after conversion
+	    cityList.add("Chennai");
+	    cityList.add("Delhi");
+
+	    //print ArrayList elements using advanced for loop
+	    for (String str: cityList)
+	    {
+	      System.out.println(str);
+	    }
+	  }
+	}
